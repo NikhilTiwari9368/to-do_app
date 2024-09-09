5a6e1cfeae987bb8842f0756202237d370519d5e
Nikhil Tiwari <nikhiltiwari9368@gmail.com>
2024-09-09 18:15:47 +0530

    the second commit

--- to-do.js
+++ to-do.js
@@ -1,42 +1,38 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
-function addTask(){
-    if(inputBox.value === ''){
-        alert("you must write somethiing!")
-    }
-    else
-    {
+
+function addTask() {
+    if (inputBox.value === '') {
+        alert("You must write something!");
+    } else {
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
-        listContainer.appendChild(li);
-
+        
         let span = document.createElement("span");
-        span.innerHTML ="\u00d7";
+        span.innerHTML = '<i class="fas fa-trash"></i>'; 
         li.appendChild(span);
-    }
 
+        listContainer.appendChild(li);
+    }
     inputBox.value = "";
     saveData();
 }
 
-listContainer.addEventListener("click", function(e){
-    if(e.target.tagName === "LI"){
+listContainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
         e.target.classList.toggle("checked");
         saveData();
-    }
-    else if(e.target.tagName === "SPAN")
-    {
-        e.target.parentElement.remove();
+    } else if (e.target.closest("span")) {  
+        e.target.closest("li").remove();
         saveData();
-    }s
+    }
 }, false);
 
-
-function saveData(){
-    localStorage.setItem("data" , listContainer.innerHTML);
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
 }
 
-function showTask(){
+function showTask() {
     listContainer.innerHTML = localStorage.getItem("data");
 }
 
